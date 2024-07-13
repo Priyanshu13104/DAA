6b8941653f523d76e95cb1e566ad420088e62b06
Priyanshu13104 <piyuprajapati13104@gmail.com>
2024-07-13 10:34:30 +0530

    greedy programs add

---
+++ Greedy Algorithms/chocolate distribution problem.py
@@ -0,0 +1,19 @@
+def get_choco(chocolates:list[int],M:int)->int:
+    N=len(chocolates)
+    if N < M:
+        return -1
+    chocolates.sort()
+    min_diff = float('inf')
+
+    i = 0
+    j = M - 1
+    while j<N:
+        min_diff = min(min_diff,chocolates[j]-chocolates[i])
+        i += 1
+        j += 1
+    return  min_diff
+
+# N = 8
+M = 5
+A = [3, 4, 1, 9, 56, 7, 9, 12]
+print(get_choco(A,M))
