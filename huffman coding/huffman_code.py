fff917ffbe9f155d4f82e2f82660a81f9cd37850
Priyanshu13104 <piyuprajapati13104@gmail.com>
2024-07-19 10:41:56 +0530

    huffman code update default dict

--- huffman coding/huffman_code.py
+++ huffman coding/huffman_code.py
@@ -1,17 +1,24 @@
-def frequency_map(s:str)->dict:
-    f = dict()
-    for x in s:
-        if x not in f:
-            f[x] = 1
-        else:
-            f[x] += 1
+# def frequency_map(s:str)->dict:
+#     f = dict()
+#     for x in s:
+#         if x not in f:
+#             f[x] = 1
+#         else:
+#             f[x] += 1
+#
+#     return f
 
+
+# default dict
+from collections import defaultdict
+
+def frequency_map(s:str)->dict:
+    f = defaultdict(int)
+    for x in s:
+        f[x] += 1
     return f
 
 print(frequency_map('Priyanshu'))
 
 
 
-
-
-# default dict
